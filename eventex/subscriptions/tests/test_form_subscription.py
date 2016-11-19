d22736f9fcea5927835eec703eeb40b53e443a01
Fernando Silva <liquuid@gmail.com>
2016-11-18 23:16:22 -0200

    Email e telefone sao opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -26,6 +26,21 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='123')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_email_is_optional(self):
+        """ Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """ Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_phone_or_mail(self):
+        """ Must inform phone or mail"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
 
     def test_name_must_be_capitalized(self):
         """ Name must be capitalized"""
