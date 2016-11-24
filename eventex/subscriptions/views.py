3ebc2026de11060a9bb03f377174377bd3516841
Fernando Silva <liquuid@gmail.com>
2016-11-24 15:12:46 -0200

    Simplifica com modelform

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,5 @@
-from django.contrib import messages
-from django.core import mail
 from django.conf import settings
+from django.core import mail
 from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
@@ -16,9 +15,11 @@ def new(request):
     else:
         return empty_form(request)
 
+
 def empty_form(request):
     return render(request, 'subscriptions/subscription_form.html',
                   {'form': SubscriptionForm()})
+
 
 def create(request):
     form = SubscriptionForm(request.POST)
@@ -27,23 +28,16 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-
-    subscription = Subscription.objects.create(**form.cleaned_data)
-
+    subscription = form.save()
 
     # Send email
     _send_email('Confirmação de inscrição',
-               settings.DEFAULT_FROM_EMAIL,
-               subscription.email,
-               'subscriptions/subscription_email.txt',
+                settings.DEFAULT_FROM_EMAIL,
+                subscription.email,
+                'subscriptions/subscription_email.txt',
                 {'subscription': subscription})
 
-
-
     return HttpResponseRedirect(resolve_url('subscriptions:detail', subscription.pk))
-
-
-
 
 
 def detail(request, pk):
@@ -54,6 +48,7 @@ def detail(request, pk):
 
     return render(request, 'subscriptions/subscription_detail.html',
                   {'subscription': subscription})
+
 
 def _send_email(subject, from_, to, template_name, context):
     body = render_to_string(template_name, context)
