39ebd1b9fec8fc5f4cb2fa4422d806f90df91fa7
Fernando Silva <liquuid@gmail.com>
2016-11-25 11:45:04 -0200

    talks

---
+++ eventex/core/tests/test_view_talk_list.py
@@ -0,0 +1,60 @@
+from django.shortcuts import resolve_url
+from django.test import TestCase
+
+from eventex.core.models import Talk, Speaker
+
+
+class TalkListGet(TestCase):
+    def setUp(self):
+
+        t1 =Talk.objects.create(title='Título da palestra',
+                            start='10:00',
+                            description='Descrição da palestra')
+        t2 = Talk.objects.create(title='Título da palestra',
+                            start='13:00',
+                            description='Descrição da palestra')
+
+        speaker = Speaker.objects.create(
+            name = 'Alan Turing',
+            slug = 'alan-turing',
+            website = 'http://hbn.link/turing-site'
+        )
+
+        t1.speakers.add(speaker)
+        t2.speakers.add(speaker)
+        self.resp = self.client.get(resolve_url('talk_list'))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'core/talk_list.html')
+
+    def test_html(self):
+        contents = [
+            (2, 'Título da palestra'),
+            (1, '10:00'),
+            (1, '13:00'),
+            (2, '/palestrantes/alan-turing/'),
+            (2, 'Alan Turing'),
+            (2, 'Descrição da palestra'),
+
+        ]
+
+        for count, expect in contents:
+            with self.subTest():
+                self.assertContains(self.resp, expect, count)
+
+    def test_context(self):
+        variables = ['morning_talks', 'afternoon_talks']
+
+        for key in variables:
+            with self.subTest():
+                self.assertIn(key, self.resp.context)
+
+class TalkListGetEmpty(TestCase):
+    def test_get_empty(self):
+        response = self.client.get(resolve_url('talk_list'))
+        self.assertContains(response, 'Ainda não existem palestras de manhã')
+        self.assertContains(response, 'Ainda não existem palestras de tarde')
+
--- eventex/urls.py
+++ eventex/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from eventex.core.views import home, speaker_detail
+from eventex.core.views import home, speaker_detail, talk_list
 
 #import eventex.core.views
 
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^$', home, name="home"),
     url(r'^inscricao/', include('eventex.subscriptions.urls',
                                 namespace='subscriptions')),
+    url(r'^palestras/$', talk_list, name='talk_list'),
     url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
     url(r'^admin/', admin.site.urls),
 ]
